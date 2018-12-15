8589324180ac99059e0936fd10c878f98d7f2179
joken0120 <joken0120@gmail.com>
2018-12-15 12:29:01 +0900

    javascript products, orders

---
+++ api/routes/orders.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /orders'
+    });
+});
+
+
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /orders'
+    });
+});
+
+
+module.exports = router;
